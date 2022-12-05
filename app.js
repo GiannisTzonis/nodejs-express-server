bf2f4fb0a310f8cb8fd02a3cf7cc49829f271f42
GiannisTzonis <tzonis.john@hotmail.com>
2022-12-05 15:53:42 +0200

    fixed post requests

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("This always works");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In Another MiddleWare");
-  res.send("<h1>The add-product page</h1>");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In Another MiddleWare");
-  res.send("<h1>hello from express</h1>");
+  res.send("<h1>Hello from Express!</h1>");
 });
 
-app.listen(4000);
+app.listen(3000);
