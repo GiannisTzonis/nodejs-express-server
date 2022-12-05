d443e4ae68a93883f3fea2298c03751e462f1429
GiannisTzonis <tzonis.john@hotmail.com>
2022-12-05 15:30:01 +0200

    added paths

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the MiddleWare");
-  next(); //this allows the request to continue to the next middleware in line
+app.use("/", (req, res, next) => {
+  console.log("This always works");
+  next();
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+  console.log("In Another MiddleWare");
+  res.send("<h1>The add-product page</h1>");
+});
+
+app.use("/", (req, res, next) => {
   console.log("In Another MiddleWare");
   res.send("<h1>hello from express</h1>");
 });
-
-// const server = http.createServer(app);
-// server.listen(4000);
 
 app.listen(4000);
