ea4b6d99a10283d4c25bbf0cc6a139c583e86776
GiannisTzonis <tzonis.john@hotmail.com>
2022-12-10 14:58:22 +0200

    added middleware

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("In the MiddleWare");
+  next(); //this allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("In Another MiddleWare");
+});
+
 const server = http.createServer(app);
 
 server.listen(4000);
